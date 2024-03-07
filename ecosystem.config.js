d89160763ecb5175d92730a48d3030e78014c702
Daniel203 <danielarduini47@gmail.com>
2024-03-07 21:38:01 +0100

    feat: added business logic layer

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,27 @@
 module.exports = {
   apps: [
     {
-      name: "backend",
+      name: "process-centric-service-layer",
       script: "app.js",
-      cwd: "./backend",
+      cwd: "./process-centric-service-layer",
       watch: true,
       ignore_watch: ["./node_modules"],
       env: {
         NODE_ENV: "development",
         PORT: 3010,
+        BUSINESS_PROCESS_SERVICE_URL: "http://localhost:3020",
+      },
+    },
+    {
+      name: "business-logic-service-layer",
+      script: "app.js",
+      cwd: "./business-logic-service-layer",
+      watch: true,
+      ignore_watch: ["./node_modules"],
+      env: {
+        NODE_ENV: "development",
+        PORT: 3020,
+        ADAPTER_SERVICE_URL: "http://localhost:3030",
       },
     },
   ],
