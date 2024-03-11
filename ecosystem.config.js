ba674960e061b7cd838722d820ccb0dc222cf244
Daniel203 <danielarduini47@gmail.com>
2024-03-11 21:59:05 +0100

    feat: added the last two layers

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,5 +24,31 @@ module.exports = {
         ADAPTER_SERVICE_URL: "http://localhost:3030",
       },
     },
+    {
+      name: "adapter-service-layer",
+      script: "app.js",
+      cwd: "./adapter-service-layer",
+      watch: true,
+      ignore_watch: ["./node_modules"],
+      env: {
+        NODE_ENV: "development",
+        PORT: 3030,
+        DATA_SERVICE_LAYER: "http://localhost:3040",
+      },
+    },
+    {
+      name: "data-service-layer",
+      script: "app.js",
+      cwd: "./data-service-layer",
+      watch: true,
+      ignore_watch: ["./node_modules"],
+      env: {
+        NODE_ENV: "development",
+        PORT: 3040,
+        SUPABASE_URL: "https://ippynrgkwygyysedwjyy.supabase.co",
+        SUPABASE_KEY:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwcHlucmdrd3lneXlzZWR3anl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkyMDU0NDMsImV4cCI6MjAyNDc4MTQ0M30.5eV0v0pnVxtBPmVw5XDnHU1iB3JKVibMSL9UUkbhAbY",
+      },
+    },
   ],
 };
